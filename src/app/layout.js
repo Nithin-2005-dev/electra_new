d401ca23a01f94c31a0b849b809923c8be12cca1
Nithin-2005-dev <nithinkumar2005b@gmail.com>
2025-11-05 11:46:09 +0530

    site map added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,11 +7,13 @@ export const metadata = {
   title: "Electra Society",
   description: `Discover the Electra Society of NIT Silchar. Explore event galleries, academic resources, team details, and all about the Electrical Engineering Department.`,
   keywords:`Electrical Engineering, NIT Silchar, academic resources, class notes, electrical engineering department, engineering seminars, technical workshops, research publications, student activities, Electra Society, NIT Silchar events, faculty directory, lab manuals, project reports, engineering syllabus, engineering assignments,Electra Society NIT Silchar,Electrical Engineering Site Nit Silchar`,
+  icons: {
+    icon: "https://res.cloudinary.com/dqa3ov76r/image/upload/v1729535834/favoritePhotos/ldlchr4ijcpgfq8nu2gx.jpg",
+  },
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <link rel="icon" href="/electra_logo.jpg" />
       <body className="bg-background text-textPrimary antialiased">
         <Header />
         <BackgroundElectric />
